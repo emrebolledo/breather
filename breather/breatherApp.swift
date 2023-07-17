73d87eee704489887319299c53cd278a288b1bf1
Emma Rebolledo <emrebolledo17@gmail.com>
2023-07-17 15:16:08 -0500

    Initial Commit

---
+++ breather/breatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  breatherApp.swift
+//  breather
+//
+//  Created by Scholar on 7/17/23.
+//
+
+import SwiftUI
+
+@main
+struct breatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
