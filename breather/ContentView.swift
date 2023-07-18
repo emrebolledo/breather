35ded1f466fa219e2f524bda64cfee62ce7c1513
Emma Rebolledo <emrebolledo17@gmail.com>
2023-07-18 10:20:50 -0500

    yea

--- breather/ContentView.swift
+++ breather/ContentView.swift
@@ -10,10 +10,14 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Image("logo")
+            Text("loading screen..")
+                .font(.largeTitle)
+              .fontWeight(.semibold)
+            
+            NavigationLink(destination: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Destination@*/Text("Destination")/*@END_MENU_TOKEN@*/) {
+                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Label Content@*/Text("Navigate")/*@END_MENU_TOKEN@*/
+            }
         }
         .padding()
     }
