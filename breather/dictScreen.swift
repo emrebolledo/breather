8cea13a4b08e32c8d260c5e0758a7540c838abdb
Emma Rebolledo <emrebolledo17@gmail.com>
2023-07-19 11:21:05 -0500

    yuppie

--- breather/dictScreen.swift
+++ breather/dictScreen.swift
@@ -11,7 +11,7 @@ struct dictScreen: View {
     var body: some View {
         VStack{
             Text("Dictonary")
-                .Spacer
+                .padding()
             Text("")
         
         }
